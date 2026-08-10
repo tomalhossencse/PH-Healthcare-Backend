2ecd2e4e78d7a8bdd61258d0e119d206edfe5767
Tomal Hossen <tomalhossen78@gmail.com>
2026-08-10 20:03:53 +0600

    feat: add LoginZodSchema for user login validation and update login route to use it

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -1,7 +1,7 @@
 import { auth } from "../../middleware/checkAuth";
 import { AuthController } from "./auth.controller";
 import { Role } from "../../../generated/prisma/enums";
-import { PataintRegZodSchema } from "./auth.validation";
+import { LoginZodSchema, PataintRegZodSchema } from "./auth.validation";
 import { Router } from "express";
 import { validationRequest } from "../../middleware/validateRequest";
 
@@ -12,7 +12,11 @@ router.post(
 	validationRequest(PataintRegZodSchema),
 	AuthController.registerPatient,
 );
-router.post("/login", AuthController.loginUser);
+router.post(
+	"/login",
+	validationRequest(LoginZodSchema),
+	AuthController.loginUser,
+);
 router.get(
 	"/me",
 	auth(Role.ADMIN, Role.DOCTOR, Role.PATIENT, Role.SUPER_ADMIN),
