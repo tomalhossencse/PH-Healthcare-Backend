8a31b6f83766f4e83276ceefbebab51cfda13874
Tomal Hossen <tomalhossen78@gmail.com>
2026-08-31 13:51:57 +0600

    feat: add analytics routes to the application

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,7 @@ import { DoctorRoutes } from "./app/module/doctor/doctor.route";
 import { ScheduleRoutes } from "./app/module/schedule/schedule.route";
 import { PrescriptionRoutes } from "./app/module/prescription/prescription.route";
 import { PaymentRoutes } from "./app/module/payment/payment.route";
+import { AnalyticsRoutes } from "./app/module/analytics/analytics.route";
 
 const app: Application = express();
 
@@ -42,6 +43,8 @@ app.use("/api/v1/doctor", DoctorRoutes);
 app.use("/api/v1/schedule", ScheduleRoutes);
 app.use("/api/v1/payment", PaymentRoutes);
 app.use("/api/v1/prescription", PrescriptionRoutes);
+app.use("/api/v1/prescription", PrescriptionRoutes);
+app.use("/api/v1/analytics", AnalyticsRoutes);
 
 // app.get("/test", async (req: Request, res: Response) => {
 // 	try {
